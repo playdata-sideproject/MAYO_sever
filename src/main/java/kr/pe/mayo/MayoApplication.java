90e8a57af436b666c4dc379f0ebcb1d4cf072c26
seeun214 <seeunjin22@naver.com>
2021-11-17 22:41:26 +0900

    fix MayoApplication

--- src/main/java/kr/pe/mayo/MayoApplication.java
+++ src/main/java/kr/pe/mayo/MayoApplication.java
@@ -2,11 +2,13 @@ package kr.pe.mayo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
 @SpringBootApplication
 public class MayoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MayoApplication.class, args);
 	}
 
